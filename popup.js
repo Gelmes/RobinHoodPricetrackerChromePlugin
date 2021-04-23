4095aefb8532b479a92cdb7e9529effdcdf69d28
Gelmes <marco.firsteye@gmail.com>
2021-04-23 09:46:01 -0700

    Working price summary

--- popup.js
+++ popup.js
@@ -12,12 +12,17 @@ changeColor.addEventListener("click", async () => {
       target: { tabId: tab.id },
       function: setPageBackgroundColor,
     });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
+
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['transactions.js'],
     });
-  }
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
---
+++ transactions.js
@@ -0,0 +1,45 @@
+console.log("===== Transactions =====");
+
+class Transaction {
+    constructor(element) {
+        this.type = element.children[0].children[0].children[0].children[0].children[2].children[0].innerHTML.split(" ")[1].toLowerCase();
+        this.dateTime = new Date(element.children[0].children[0].children[0].children[4].children[2].innerHTML);
+
+        let temp = element.children[0].children[0].children[0].children[5].children[2].innerHTML.split(" ");
+        this.qty = Number(temp[0].replace(/[^0-9.-]+/g,""));
+        this.price = Number(temp[3].replace(/[^0-9.-]+/g,""));
+    }
+};
+
+function printPrices() {
+    let prices = document.querySelectorAll('[data-testid="rh-ExpandableItem-content"]');
+    let transactions = [];
+    for (let i = 0; i < prices.length; i++) {
+        let transaction = new Transaction(prices[i]);
+        transactions.push(transaction);
+    }
+
+    transactions.sort(function(a,b){
+        return a.dateTime - b.dateTime;
+    });
+
+    let totaCost = 0.0;
+    let totaQty = 0.0;
+    for (let i = 0; i < transactions.length; i++) {
+        let transaction = transactions[i];
+        let cost = transaction.qty*transaction.price;
+        // console.log(transaction.dateTime );
+        if(transaction.type == "buy"){
+            totaCost -= cost;
+            totaQty -= transaction.qty;
+        } else{
+            totaCost += cost;
+            totaQty += transaction.qty;
+        }
+        console.log(transaction.type, cost.toFixed(2), totaCost.toFixed(2), transaction.qty, totaQty);
+    }
+
+}
+
+printPrices();
+
