312f98e5d0a4b758895301b33600aaa7cf075b22
Gelmes <marco.firsteye@gmail.com>
2021-05-08 17:36:14 -0700

    Added output to html support

--- orders.js
+++ orders.js
@@ -14,10 +14,36 @@ class Order {
     }
 };
 
+class TableMaker {
+    constructor(){
+        
+        this.content = document.createElement("table");
+        this.content.classList.add("outputTable");
+        this.rows = [];
+    }
+    addRow(){
+        let row = document.createElement("tr");
+        row.classList.add("outputContentRow");
+        this.content.appendChild(row);
+        this.rows.push(row);
+        return this.rows.length - 1;
+    }
+    addColumn(row, content){
+        let column = document.createElement("td");
+        column.classList.add("outputContentColumn");
+        this.rows[row].appendChild(column);
+        column.innerHTML = content;
+    }
+}
+
 function printOrders() {
     let prices = document.querySelectorAll('[data-testid="rh-ExpandableItem-content"]');
+    let output = document.getElementsByClassName("main-container")[0];
+    let outputDiv = document.createElement("div");
+    output.insertBefore(outputDiv, output.childNodes[0]);
+
     let orders = [];
-    for (let i = 0; i < prices.length; i++) {
+    for (let i = 0; i < prices.length; i++){
         let order = new Order(prices[i]);
         orders.push(order);
     }
@@ -27,22 +53,95 @@ function printOrders() {
         return b.price - a.price;
     });
 
+    let table = new TableMaker();
+    let totalBuy = 0;
+    let totalSell = 0;
+    let totalBuyQty = 0;
+    let totalSellQty = 0;
+
+    let rowNumber = table.addRow();
+    table.addColumn(rowNumber, "Buy");
+    table.addColumn(rowNumber, "Type");
+    table.addColumn(rowNumber, "Status");
+    table.addColumn(rowNumber, "Price");
+    table.addColumn(rowNumber, "Cost");
+    table.addColumn(rowNumber, "Quantity");
+
     console.log("\n===== Buy Limit Orders =====");
     for (let i = 0; i < orders.length; i++) {
         let order = orders[i];
         if(order.limit == "limit" && order.status == "confirmed" && order.type == "buy"){
-            console.log(order.limit, '\t$', order.type, '\t', order.status, '\t$', order.price.toFixed(4),'\t$',  order.cost.toFixed(2),'\t', order.qty.toFixed(2));
+        // if(order.limit == "limit" && order.type == "buy"){
+            let result = order.limit + "\t" + order.type + "\t" + order.status + "\t$" + order.price.toFixed(4) + "\t$" + order.cost.toFixed(2) + "\t" + order.qty.toFixed(2);
+            console.log(result);
+            let rowNumber = table.addRow();
+            table.addColumn(rowNumber, order.limit);
+            table.addColumn(rowNumber, order.type);
+            table.addColumn(rowNumber, order.status);
+            table.addColumn(rowNumber, order.price.toFixed(4));
+            table.addColumn(rowNumber, order.cost.toFixed(2));
+            table.addColumn(rowNumber, order.qty.toFixed(2));
+
+            totalBuy += order.cost;
+            totalBuyQty += order.qty;
         }
     }
+
+    rowNumber = table.addRow();
+    table.addColumn(rowNumber, "Totals");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, totalBuy);
+    table.addColumn(rowNumber, totalBuyQty);
+
+    rowNumber = table.addRow();
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+
+    
+    rowNumber = table.addRow();
+    table.addColumn(rowNumber, "Sell");
+    table.addColumn(rowNumber, "Type");
+    table.addColumn(rowNumber, "Status");
+    table.addColumn(rowNumber, "Price");
+    table.addColumn(rowNumber, "Cost");
+    table.addColumn(rowNumber, "Quantity");
 
     console.log("\n===== Buy Sell Orders =====");
     for (let i = 0; i < orders.length; i++) {
         let order = orders[i];
         if(order.limit == "limit" && order.status == "confirmed" && order.type == "sell"){
-            console.log(order.limit, '\t$', order.type, '\t', order.status, '\t$', order.price.toFixed(4),'\t$',  order.cost.toFixed(2),'\t', order.qty.toFixed(2));
+        // if(order.limit == "limit" && order.type == "sell"){
+            let result = order.limit + "\t" + order.type + "\t" + order.status + "\t$" + order.price.toFixed(4) + "\t$" + order.cost.toFixed(2) + "\t" + order.qty.toFixed(2);
+            console.log(result);
+            
+            let rowNumber = table.addRow();
+            table.addColumn(rowNumber, order.limit);
+            table.addColumn(rowNumber, order.type);
+            table.addColumn(rowNumber, order.status);
+            table.addColumn(rowNumber, order.price.toFixed(4));
+            table.addColumn(rowNumber, order.cost.toFixed(2));
+            table.addColumn(rowNumber, order.qty.toFixed(2));
+
+            totalSell += order.cost;
+            totalSellQty += order.qty;
         }
     }
 
+    rowNumber = table.addRow();
+    table.addColumn(rowNumber, "Totals");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, "   ");
+    table.addColumn(rowNumber, totalSell);
+    table.addColumn(rowNumber, totalSellQty);
+
+    outputDiv.appendChild(table.content);
 }
 
 printOrders();
--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById("changeColor");
+let seeOrders = document.getElementById("seeOrders");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
@@ -6,17 +7,33 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['transactions.js'],
+  });
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+seeOrders.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
 
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      files: ['transactions.js'],
+      files: ['orders.js'],
     });
+    
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ['table.css'],
+    });
+
+  
 });
 
 // The body of this function will be executed as a content script inside the
